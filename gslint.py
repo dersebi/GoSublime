412ba198a6f920d535f09de19a67f208771c490a
Sebastian Schlingmann <sebastian.schlingmann@web.de>
2011-10-06 07:58:55 +0200

    gslint runs on all files in the same package in the current dir

--- gslint.py
+++ gslint.py
@@ -1,6 +1,8 @@
 import sublime, sublime_plugin
 import gscommon as gs
 import re, threading
+import os
+import string
 
 LINE_PAT = re.compile(r':(\d+):(\d+):\s+(.+)\s*$', re.MULTILINE)
 
@@ -41,13 +43,60 @@ class GsLint(sublime_plugin.EventListener):
     def on_load(self, view):
         self.on_modified(view)
     
+    #taken from http://stackoverflow.com/questions/241327/python-snippet-to-remove-c-and-c-comments/241506#241506
+    def comment_remover(self, text):
+        def replacer(match):
+            s = match.group(0)
+            if s.startswith('/'):
+                return ""
+            else:
+                return s
+        pattern = re.compile(
+            r'//.*?$|/\*.*?\*/|\'(?:\\.|[^\\\'])*\'|"(?:\\.|[^\\"])*"',
+            re.DOTALL | re.MULTILINE
+        )
+        return re.sub(pattern, replacer, text)
+
+    #extracts the go package name from a go source file
+    def extract_go_package_name(self, filename):
+        fd = open(filename, "r")
+        content = string.join(fd.readlines())
+        fd.close()
+        strippedcontent = self.comment_remover(content)
+        for line in strippedcontent.split("\n"):
+            sline = line.strip()
+            if sline.find("package") == 0:
+                return sline.split(" ")[1]
+        return ""   
+
+    def generate_filelist_for_lint(self, view):
+        #input filename
+        filename = view.file_name()
+        #list of files to run lint on
+        lintfiles = [filename]
+        basepath = os.path.dirname(filename)
+        packagename = self.extract_go_package_name(filename)
+
+        for name in os.listdir(basepath):
+            newfile = os.path.join(basepath,name)
+            if newfile == filename:
+                continue
+            if os.path.isfile(newfile):
+                _, ext = os.path.splitext(name)
+                if ext.lower() == ".go":
+                    if self.extract_go_package_name(newfile) == packagename:
+                        lintfiles += [newfile]
+        return lintfiles
+
     def lint(self, view):
         self.rc -= 1
 
         if self.rc == 0:
             cmd = gs.setting('gslint_cmd', 'gotype')
             if cmd:
-                _, err = gs.runcmd([cmd], view.substr(sublime.Region(0, view.size())))
+                filelist = self.generate_filelist_for_lint(view)
+                filelist = [cmd] + filelist
+                _, err = gs.runcmd(filelist, "")
             else:
                 err = ''
             lines = LINE_PAT.findall(err)
